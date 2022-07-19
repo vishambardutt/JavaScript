583f80957934327f96568b49707375e57661fae1
vishambar <vishambardj@yahoo.com>
2022-07-19 12:48:45 +0530

    js files added

---
+++ ternary-operator.js
@@ -0,0 +1,15 @@
+let age = 20;
+let result 
+if (age >= 18) {
+  result = "You are eligible to vote.";
+} else {
+  result = "You are not eligible to vote yet.";
+}
+
+console.log(result);
+
+// ternary operator to check the eligibility to vote
+// let age1 = 15;
+// let result1 =
+//     (age1 >= 18) ? "You are eligible to vote." : "You are not eligible to vote yet";
+// console.log(result1);
